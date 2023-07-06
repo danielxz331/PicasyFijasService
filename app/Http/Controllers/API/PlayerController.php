36cab2749c121cba3358b794ef220f5bb8f4e242
danielxz331 <Tu direccin de correo electrnico>
2023-07-06 17:08:08 -0500

    Login finalizado

--- app/Http/Controllers/API/PlayerController.php
+++ app/Http/Controllers/API/PlayerController.php
@@ -15,10 +15,30 @@ class PlayerController extends Controller
             'number' => 'required|size:4'
         ]);
 
-        // La lógica para asignar el turno aleatoriamente se deja a tu consideración.
-
-        $player = Player::create($request->all());
-
+    
+        $player = new Player();
+    
+        $player->name = $request->name;
+        $player->number = $request->number;
+    
+        // Obtenemos todos los turnos ya usados
+        $usedTurns = Player::pluck('turn')->toArray();
+    
+        // Creamos un array con los números del 1 al 4
+        $allTurns = range(1, 4);
+    
+        // Obtenemos los turnos aún disponibles
+        $availableTurns = array_diff($allTurns, $usedTurns);
+    
+        if(empty($availableTurns)) {
+            return response()->json(['error' => 'Ya hay 4 jugadores en el juego.'], 400);
+        }
+    
+        // Seleccionamos aleatoriamente uno de los turnos disponibles
+        $player->turn = $availableTurns[array_rand($availableTurns)];
+    
+        $player->save();
+    
         return response()->json($player, 201);
     }
 }
